e5a49234d036f5361017e6a50708aa7de8ee6e00
Sala Digital (CtP/ETS) <disrct@bosch.com>
2023-07-05 08:26:43 -0300

    DB novo computador

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,8 @@
 const sequelize = require('sequelize');
 //configurações da base de dados
-const database = new sequelize('PortalAlunos', 'admin', 'admin1234567',
+const database = new sequelize('PortalAlunos', 'disrct', 'etstech31415',
 {
-dialect: 'mssql', host:'localhost', port: 1433
+dialect: 'mssql', host:'localhost', port: 57136
 });
 database.sync();
 module.exports = database;

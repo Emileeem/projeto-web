20f84972cd5c96f3fc6c5cd06148432687183a29
Benhur Feld <benhurfeld@gmail.com>
2023-07-05 12:56:27 -0300

    Tela aluno Tabela inner join

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,31 +1,42 @@
-const turma = require('../model/turma'); 
-const professor = require('../model/professor');
-const aluno = require('../model/aluno');
-const turmaMateria = require('../model/turmaMateria');
-const materia = require('../model/materia');
+const turma = require("../model/turma");
+const professor = require("../model/professor");
+const aluno = require("../model/aluno");
+const turmaMateria = require("../model/turmaMateria");
+const materia = require("../model/materia");
 
 module.exports = {
-    async HomeAlunoGet(req, res){
-        const DBedv = await aluno.findByPk(req.params.IDAluno, {
-            raw: true,
-            attributes: ['IDAluno', 'Nome', 'Senha', 'Foto']
-        });
-        res.render('../views/Home', {DBedv});
-    },
-    
-    async HomeProfGet(req, res){
-        const DBedv = await professor.findByPk(req.params.IDProfessor, {
-            raw: true,
-            attributes: ['IDProfessor', 'Nome', 'Senha', 'Foto']
-        });
-        const Materia = await materia.findAll({
-            raw: true,
-            where: {
-                IDProfessor: DBedv.IDProfessor,
-            }
-        });
-        console.log(Materia);
-        res.render('../views/HomeProf', {Materia, DBedv});
-    }
-}
+  async HomeAlunoGet(req, res) {
+    const DBedv = await aluno.findByPk(req.params.IDAluno, {
+      raw: true,
+      attributes: ["IDAluno", "Nome", "Senha", "Foto", "IDTurma"],
+    });
 
+    const TurmaMateria = await turmaMateria.findAll({
+        raw: true,
+        where: {
+            IDTurma: DBedv.IDTurma
+        },
+        include: [{
+            model: materia,
+            required: false,
+            attributes: [["Nome", "NomeMateria"]]
+        }]
+    });
+    console.log(TurmaMateria)
+    res.render("../views/Home", { DBedv, TurmaMateria });
+  },
+
+  async HomeProfGet(req, res) {
+    const DBedv = await professor.findByPk(req.params.IDProfessor, {
+      raw: true,
+      attributes: ["IDProfessor", "Nome", "Senha", "Foto", "IDTurma"],
+    });
+    const Materia = await materia.findAll({
+      raw: true,
+      where: {
+        IDProfessor: DBedv.IDProfessor
+      }
+    });
+    res.render("../views/HomeProf", { Materia, DBedv });
+  },
+};
